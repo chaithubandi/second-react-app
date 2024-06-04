47c47b6e91a954cf428f800c5ae2fcd41152f1bc
chaithubandi <chaithubandi108@gmail.com>
2024-06-04 21:12:42 +0530

    Add files via upload

---
+++ second-react-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>gold rates in india</h1>
+      <h2>gold rates in telangana</h2>
+      <p>The gold rates in Hyderabad on 2 June, 2024 have been stable. Going by the rates today, the price of 10 grams of 22-carat gold, 66,500 while the rate of 10 grams of 24-carat gold is trading at Rs. 72,550. As for silver, the silver rate in Hyderabad is recorded at Rs. 98,000 per kilogram. The gold rates in the international market have been fluctuating. Over the past few weeks gold rates have experienced a fall during the wedding season, fluctuating around Rs. 60,000 for 10 grams of 24-carat gold and approximately Rs. 55,000 for 10 grams of 22-carat gold. The gold prices mentioned here are due at 8 am, the prices could alter at every moment and hence the gold buyers need to track the live prices at a given time. The mentioned prices here are closing prices of yesterday while today's price would begin either with a decrease or increase.
+
+https://www.thehansindia.com/business/gold-rates-in-hyderabad-today-check-the-rates-on-2-june-2024-882316
+</p>
+    </div>
+  );
+}
+
+export default App;
